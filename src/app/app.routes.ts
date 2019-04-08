c15d0f227b03524b96aebbddc5f0501e328262f2
laurent <laurentmartins@outlook.com>
2019-04-08 17:02:59 +0200

    passage des tests

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { PATH_HOME, PATH_ARTIST, PATH_USER, PATH_EVENT,
-     PATH_LOGIN, PATH_INSCRIPTION, PATH_PROFILE, PATH_INSCRIPTION_ARTIST } from './app.routes.constantes';
+import {
+    PATH_HOME, PATH_ARTIST, PATH_USER, PATH_EVENT,
+    PATH_LOGIN, PATH_INSCRIPTION, PATH_PROFILE, PATH_INSCRIPTION_ARTIST
+} from './app.routes.constantes';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { EventComponent } from './event/event.component';
@@ -21,13 +23,15 @@ import { LoggedInGuard } from './logged-in.guard';
 export const ROUTES: Routes = [
     { path: PATH_HOME, component: HomeComponent },
     { path: PATH_LOGIN, component: LoginComponent },
-    { path: PATH_INSCRIPTION, component: InscriptionComponent,
+    {
+        path: PATH_INSCRIPTION, component: InscriptionComponent,
         children: [
-        { path: PATH_INSCRIPTION_ARTIST, component: InscriptionArtistComponent, },
-    ]},
+            { path: PATH_INSCRIPTION_ARTIST, component: InscriptionArtistComponent, },
+        ]
+    },
     { path: PATH_PROFILE, component: ProfileComponent },
     {
-        path: PATH_USER, component: UserComponent,canActivate: [LoggedInGuard],
+        path: PATH_USER, component: UserComponent, canActivate: [LoggedInGuard],
         children: [
             { path: ':id', component: UserDetailComponent, },
             { path: ':id/modification', component: UserModifComponent, }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {  PATH_HOME, PATH_USER } from '../app.routes.constantes';
+import { PATH_HOME, PATH_USER } from '../app.routes.constantes';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../user-service/user.service';
 import { LoggedInGuard } from '../logged-in.guard';
-import { error } from '@angular/compiler/src/util';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -15,34 +15,35 @@ export class LoginComponent implements OnInit {
   usernameCtrl: FormControl;
   passwordCtrl: FormControl;
   userForm: FormGroup;
-  try:boolean=false;
-  constructor(private router: Router,fb: FormBuilder,private userService:UserService,private guard:LoggedInGuard) {
+  error: boolean;
+  constructor(private router: Router, fb: FormBuilder, private userService: UserService, private guard: LoggedInGuard) {
     this.usernameCtrl = fb.control('', [Validators.required]);
-    this.passwordCtrl = fb.control('', [Validators.required]);               
+    this.passwordCtrl = fb.control('', [Validators.required]);
     this.userForm = fb.group({
       username: this.usernameCtrl,
       password: this.passwordCtrl,
-  
+
     });
-   }
+  }
   NavigateToHome() {
     this.router.navigate([PATH_HOME]);
   }
-  handleSubmit(){
-    this.try=true;
-    this.userService.login(this.usernameCtrl.value,this.passwordCtrl.value).then(token=>{
-      if(token!=undefined){
+  handleSubmit() {
+    this.userService.login(this.usernameCtrl.value, this.passwordCtrl.value).then(status => {
+      if (status === 200) {
         this.router.navigate([PATH_USER]);
+      } else {
+        this.error = true;
       }
-    }).catch()
+    });
   }
-  Cancel(){
-    this.usernameCtrl.setValue("");
-    this.passwordCtrl.setValue("");
-    this.usernameCtrl.markAsPending({onlySelf:false});
-    this.passwordCtrl.markAsPending({onlySelf:false});
+  Cancel() {
+    this.usernameCtrl.setValue('');
+    this.passwordCtrl.setValue('');
+    this.usernameCtrl.markAsPending({ onlySelf: false });
+    this.passwordCtrl.markAsPending({ onlySelf: false });
   }
   ngOnInit() {
-    this.try=false
+    this.error = false;
   }
 }
